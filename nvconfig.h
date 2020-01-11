07fe79e70b7f8d430ffa6ec5b459ce614fdb0600
Fredrik Lindahl <fredrik-lindahl@outlook.com>
2020-01-11 15:53:00 +0100

    rewrote cmakelists to use fips. Added nvconfig.h to root directory Added fips-stb import for latests version Changed submodule URL to a fork of NVTT that implements latest stb image version

---
+++ nvconfig.h
@@ -0,0 +1,11 @@
+#ifndef NV_CONFIG
+#define NV_CONFIG
+
+#define NV_HAVE_STBIMAGE
+// #define HAVE_PNG
+// #define HAVE_JPEG
+// #define HAVE_TIFF
+// #define HAVE_OPENEXR
+// #define HAVE_FREEIMAGE
+
+#endif // NV_CONFIG
